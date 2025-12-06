4e8f0adace180ab97388f8bc408bac5e158a62e0
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-06 19:54:29 +0400

    added logics and main visual so how it will look like when it will be integrated on website

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,13 +1,14 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import ChatHeader from "./chat/ChatHeader";
 import ChatMessages from "./chat/ChatMessages";
 import { ChatInput } from "./chat/ChatInput";
 import { Message } from "./chat/types";
+import { MessageCircle, X } from "lucide-react";
 
 const ChatWidget = () => {
-  const onClose = () => {};
-  const messages: Message[] = [
+  const [isOpen, setIsOpen] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([
     { id: 1, text: "how are you?", sender: "user", type: "rating" }, //an example
     {
       id: 2,
@@ -15,10 +16,24 @@ const ChatWidget = () => {
       sender: "bot",
       type: "rating",
     }, //an example
-  ];
+  ]);
+  //open close chat
+  const toggleChat = () => setIsOpen(!isOpen);
+  const handleSend = (text: string) => {
+    const newUserMessage: Message = {
+      id: Date.now(),
+      text: text,
+      sender: "user",
+      type: "text",
+    };
+    setMessages((prev) => [...prev, newUserMessage]);
+    //here should add bot answer in the future
+  };
+  const onClose = () => {};
+
   const onSend = () => {};
   return (
-    <div>
+    <div className="fixed bottom-6 z-50 flex flex-col items-end font-sans">
       <ChatHeader onClose={onClose} />
       <ChatMessages messages={messages} />
       <ChatInput onSend={onSend} />
