fec163ddc6ecdfc4db47942d1986bde8a6e55111
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-06 18:58:23 +0400

    most of the logic is written now style and adjusting it

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Paperclip } from "lucide-react";
 
 interface InputProps {
@@ -6,12 +6,27 @@ interface InputProps {
 }
 
 export const ChatInput = ({ onSend }: InputProps) => {
+  const [text, setText] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    //it will not reload by this
+    e.preventDefault();
+    if (!text.trim()) return;
+    onSend(text); // the text will be send if anything is written
+    setText(""); // text will be ready to rewrite (it will be empty for now)
+  };
+
   return (
     <div>
       {/* paperClip */}
       <button>
         <Paperclip size={20} />
       </button>
+      {/* form */}
+      <form>
+        <input type="text" placeholder="write your question..." />
+      </form>
+      {/* conditional button (only if text is written even one) if not than show mic */}
+      {}
     </div>
   );
 };
